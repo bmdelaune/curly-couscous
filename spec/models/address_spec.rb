9076cb02bbf459f8e730c7eeea46c2d22b3de7c2
Bradley Delaune <bradley@delaune.me>
2025-03-24 08:41:07 -0500

    address spec complete

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,13 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
+  subject { build(:address) }
 
-  describe ".postal_code" do
-
-    it "allows storing a postal code" do
-      addr = Address.new(postal_code: "76666")
-      expect(addr.postal_code).to eq("76666")
+  describe "new" do
+    %i(street city state country postal_code).each do |attribute|
+      let(:value) { Faker::Alphanumeric.alphanumeric }
+      it "allows initializing with #{attribute}" do
+        addr = Address.new("#{attribute}": value)
+        expect(addr.send(attribute)).to eq(value)
+      end
     end
   end
 
+  describe "#full_address" do
+    let(:street) { Faker::Address.street_address }
+    let(:city) { Faker::Address.city }
+    let(:state) { Faker::Address.state_abbr }
+    let(:postal_code) { Faker::Address.zip }
+    let(:country) { Faker::Address.country }
+
+    subject { build(:address, street:, city:, state:, postal_code:, country:) }
+
+    it "returns the full address" do
+      expect(subject.full_address).to eq("#{street}, #{city}, #{state} #{postal_code}, #{country}")
+    end
+  end
+
+  describe "#lat_long" do
+    let(:latitude) { Faker::Address.latitude }
+    let(:longitude) { Faker::Address.longitude }
+
+    before do
+      allow(OpenMateo).to receive(:geocode).with(subject.full_address).and_return([latitude, longitude])
+    end
+
+    it "retrieves from OpenMateo" do
+      lat, long = subject.lat_long
+
+      expect(lat).to eq(latitude)
+      expect(long).to eq(longitude)
+      expect(OpenMateo).to have_received(:geocode).with(subject.full_address)
+    end
+  end
 end
